45307978e1cff4bf3f70818bcd4db339356a8810
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-15 00:55:56 +0200

    Make changes

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -2,7 +2,9 @@
 
 FactoryBot.define do
   factory :food do
-    name { 'Example Food' }
+    name { 'Pizza' }
+    measurement_unit { 'Slice' }
+    price { 10 }
     # Add any other attributes you need for your Food model
   end
 end
