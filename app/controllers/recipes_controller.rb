8df134e4cf8733ea213d8e27971a9c06e51ff44b
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-14 08:08:55 +0200

    make changes to public recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,8 +20,15 @@ class RecipesController < ApplicationController
     @recipes = current_user.recipes.all
   end
 
+  def generate_shopping_list
+    @recipe = Recipe.find(params[:id])
+    @missing_food_items = @recipe.missing_food_items(current_user.foods)
+    @total_missing_price = @missing_food_items.sum(&:price)
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
+    @user = current_user    
   end
 
   def destroy
@@ -32,6 +39,68 @@ class RecipesController < ApplicationController
     else
       redirect_to recipes_path, alert: 'You are not authorized to delete this recipe.'
     end
+  endclass RecipesController < ApplicationController
+  load_and_authorize_resource
+  before_action :authenticate_user!
+  def new
+    @recipe = Recipe.new
+    @users = User.all
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    if @recipe.save
+      flash[:notice] = 'Recipe saved successfully'
+    else
+      flash[:alert] = 'Recipe failed to save'
+    end
+    redirect_to recipes_path
+  end
+
+  def index
+    @recipes = current_user.recipes.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @user = current_user
+    @recipe_foods = @recipe.recipe_foods
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.user == current_user
+      @recipe.destroy
+      redirect_to recipes_path, notice: 'Recipe was successfully deleted.'
+    else
+      redirect_to recipes_path, alert: 'You are not authorized to delete this recipe.'
+    end
+  end
+
+  def toggle_public
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(public: !@recipe.public)
+    redirect_to @recipe
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :name,
+      :preparation_time,
+      :cooking_time,
+      :description,
+      :public,
+      :user_id
+    )
+  end
+end
+
+  def toggle_public
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(public: !@recipe.public)
+    redirect_to @recipe
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   has_many :foods
 
   validates :name, presence: true
+  def count_food_items
+    foods.count
+  end
+  def total_price
+    foods.sum(:price)
+  end
 end
