327380b3ba6b1416f63890b644e79170a465efd1
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-09-14 22:08:22 +0500

    fix the linter error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,16 +56,17 @@ RSpec.configure do |config|
   #     RSpec.describe UsersController, type: :controller do
   #       # ...
   #     end
+  # rubocop:disable Lint/ShadowingOuterLocalVariable
+  RSpec.configure do |config|
+    config.include FactoryBot::Syntax::Methods
+  end
+  # rubocop:enable Lint/ShadowingOuterLocalVariable
   #
   # The different available types are documented in the features, such as in
   # https://rspec.info/features/6-0/rspec-rails
   config.infer_spec_type_from_file_location!
-
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  RSpec.configure do |config|
-    config.include FactoryBot::Syntax::Methods
-  end
 end
